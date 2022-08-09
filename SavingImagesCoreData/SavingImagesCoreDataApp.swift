86fb1dbce8dd5e7d7b7c9b344c372f45a8120f73
kejlor <woj450@gmail.com>
2022-08-09 19:54:19 +0200

    Initial Commit

---
+++ SavingImagesCoreData/SavingImagesCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingImagesCoreDataApp.swift
+//  SavingImagesCoreData
+//
+//  Created by Bartosz Wojtkowiak on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SavingImagesCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
